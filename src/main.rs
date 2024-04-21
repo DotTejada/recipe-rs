6ec571807cecd71fea527c8462cf7c414e44be87
DotTejada <151406881+DotTejada@users.noreply.github.com>
2024-04-20 20:42:09 -0500

    Added command line capability, <scale> <path> < -h or -t>

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+use std::env::args;
+use std::env::current_dir;
 use std::fs;
 use std::fs::write;
+use std::process;
 use fancy_regex::Regex;
 use recipe::frac_to_scaled;
 use recipe::input_to_scaled;
@@ -24,18 +27,44 @@ fn scale_up(nums: &Vec<String>, scale: i32) -> Vec<String> {
     scaled_list
 }
 
+fn get_currdir_path(file_path: &String) -> String {
+    let slash: usize = match file_path.rfind(|c: char| c == '\\' || c == '/') {
+        Some(s) => s + 1,
+        None => 0,
+    };
+    let fpath = file_path[slash..file_path.len()].to_string();
+    let dir = current_dir().unwrap().to_str().unwrap().to_string();
+    let prune = fpath.len() - 4;
+    let mut outfile = format!("{}\\{}scaled.txt", dir, fpath[0..prune].to_string());
+    outfile = outfile.replace("/", "\\");
+    return outfile
+}
+
+fn get_recipedir_path(file_path: &String) -> String {
+    let fpath = file_path[0..file_path.len()].to_string();
+    let prune = fpath.len() - 4;
+    let outfile = format!("{}scaled.txt", fpath[0..prune].to_string());
+    return outfile
+}
+
 fn main() {
 
-    let file_path = String::from("src/cookierecipe.txt");
+    let args: Vec<String> = args().collect();
+    if args.len() < 4 {
+        eprintln!("Not enough arguments\n
+Please use with 'recipe <scale factor> <path to recipe> < -h or -t>'\n
+-h for output in exe dir, -t for output in input dir\n
+Make sure that the scale factor is > 0");
+        process::exit(1);
+    }
 
+    let scale: i32 = args[1].parse().unwrap();
+    let file_path = &args[2];
+    let path = &args[3];
     let input = fs::read_to_string(file_path)
         .expect("Should have been able to read the input");
 
     //println!("{}", input);
-
-    //let lines: Vec<String> = input.lines().map(|c| c.to_string()).collect();
-
-    let scale: i32 = 2;
 
     let re = Regex::new(r"(?m)([0-9]\/[0-9])|(\d+ [0-9]\/[0-9])|^(\d+)").unwrap();
     let nums = find_numbers(re, &input);
@@ -48,8 +77,21 @@ fn main() {
     //println!("-----------------------------" );
     //println!("{}", scaled_input);
 
-    let output = format!("Original:\r\n{}\r\n-----------------------------\r\nScale of: {}\r\n{}", input, scale, scaled_input);
-    println!("{}", output);
-    let _ = write("output.txt", output); 
+    let output = format!("Original:\r\n{}\r\n-----------------------------\r\nScale of: {}\r\n{}\n", input, scale, scaled_input);
+    println!("{output}");
+
+    if path == "-h" {
+        let outfile = get_currdir_path(file_path);
+        println!("Saved to: {}", outfile);
+        let _ = write(outfile, output); 
+    } else if path == "-t" {
+        let outfile = get_recipedir_path(file_path);
+        println!("Saved to: {}", outfile);
+        let _ = write(outfile, output); 
+    } else {
+        eprintln!("Last arg not specified: should be either -h or -t\n 
+-h for output in exe dir, -t for output in input dir");
+        process::exit(1);
+    }
 
 }
